cf0e0dd3e7d590cf49ad406e6cb20efb295b18ae
Daniel Münch <dani.munch@gmail.com>
2015-12-08 22:27:51 +0100

    Refactor tests by using currying. See also http://blog.patrikstorm.com/function-currying-in-elixir

--- users/test/users_test.exs
+++ users/test/users_test.exs
@@ -3,19 +3,24 @@ defmodule APITest do
   use Maru.Router
   use Maru.Test, for: Users.Api
 
+  import Curry
+
   @store Application.get_env(:users, :users_store)
 
   setup do
     @store.clear
   end
 
-  def get(url, type) do
-    conn(:get, "/users") 
+  def get_and_decode_(type, url) do
+    conn(:get, url) 
       |> make_response
       |> (fn r -> r.resp_body end).()
-      #|> (fn json -> Poison.decode!(json, :as type) end).()
+      |> (fn json -> Poison.decode!(json, as: type) end).()
   end
- 
+  def get_and_decode do
+    curry(&(get_and_decode_(&1, &2))) 
+  end
+
   test "/users" do 
     user1 = %User{name: "daniel", description: "developer"}
     user2 = %User{name: "hans", description: "user"}
@@ -23,10 +28,8 @@ defmodule APITest do
     user1 = %{user1 | id: @store.insert(user1)}
     user2 = %{user2 | id: @store.insert(user2)}
  
-    resp_body = conn(:get, "/users") |> make_response |> (fn r -> r.resp_body end).()
-    
     #sort the lists since by specs we don't care about the order
-    assert [user1, user2] |> Enum.sort == Poison.decode!(resp_body, as: [User]) |> Enum.sort 
+    assert [user1, user2] |> Enum.sort == "/users" |> get_and_decode.([User]).() |> Enum.sort 
   end
 
   test "/users/:id" do
@@ -36,10 +39,7 @@ defmodule APITest do
     user1 = %{user1 | id: @store.insert(user1)}
     user2 = %{user2 | id: @store.insert(user2)}
 
-    resp_body = conn(:get, "/users/#{user1.id}") |> make_response |> (fn r -> r.resp_body end).()
-    assert user1 === Poison.decode!(resp_body, as: User)
-    
-    resp_body = conn(:get, "/users/#{user2.id}") |> make_response |> (fn r -> r.resp_body end).()
-    assert user2 === Poison.decode!(resp_body, as: User)
+    assert user1 == "/users/#{user1.id}" |> get_and_decode.(User).()
+    assert user2 == "/users/#{user2.id}" |> get_and_decode.(User).()
   end
 end
